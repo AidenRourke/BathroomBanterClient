5b5bc9e0d2d5cc6f6b202c855d9ced1c61a38131
AidenRourke <aidenrourke@gmail.com>
2018-01-19 13:03:43 -0500

    Added base code

--- src/index.js
+++ src/index.js
@@ -1,8 +1,24 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware } from 'redux';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import promise from 'redux-promise';
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+import reducers from './reducers';
+
+import MainPage from './components/main_page';
+
+const createStoreWithMiddleware = applyMiddleware(promise)(createStore);
+
+ReactDOM.render(
+  <Provider store={createStoreWithMiddleware(reducers)}>
+    <BrowserRouter>
+      <div>
+        <Switch>
+          <Route path='/' component={MainPage} />
+        </Switch>
+      </div>
+    </BrowserRouter>
+  </Provider>
+  , document.getElementById('root'));
