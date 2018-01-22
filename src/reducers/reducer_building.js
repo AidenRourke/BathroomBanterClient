543117a4329452979451fab92c1a115b3702e720
AidenRourke <aidenrourke@gmail.com>
2018-01-21 22:47:15 -0500

    Done for Now

--- src/reducers/reducer_building.js
+++ src/reducers/reducer_building.js
@@ -3,7 +3,7 @@ import { STORE_BUILDING } from '../actions/index';
 export default function (state = null, action) {
 
   switch (action.type) {
-    
+
     case STORE_BUILDING:
       return action.payload;
 
